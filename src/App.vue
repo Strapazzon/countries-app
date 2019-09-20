f123424d6ed9e79d93823306293c0f26be7184a4
Getúlio Strapazzon <strapazzon09@gmail.com>
2019-09-20 15:52:38 -0300

    Implementação de Themes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" class="dark">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
+
     <router-view/>
   </div>
 </template>
@@ -12,8 +13,28 @@
 @import url('https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined');
 @import url('https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,800&display=swap');
 
+@import './scss/global.scss';
+@import './scss/themes.scss';
+@import './scss/themify.scss';
+
+body {
+  @include themify(null, $themes) {
+    background-color: theme(background)
+  }
+}
+
 #app {
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+
   font-family: 'Nunito Sans', sans-serif;
-  
+  @include themify(null, $themes) {
+    background-color: theme(background);
+    color: theme(color)
+  }
+
+  display: flex;
+  flex-direction: column;
 }
 </style>
--- src/environments.js
+++ src/environments.js
@@ -1,4 +1,5 @@
 'use strict'
+
 module.exports = {
   URL_API: 'https://restcountries.eu/rest/v2'
 }
