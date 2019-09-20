0fd08d6176250a7eacf091769d616c2ab1a4c8c0
Getúlio Strapazzon <strapazzon09@gmail.com>
2019-09-20 17:11:06 -0300

    Add navBar component

---
+++ src/components/navBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="navbar">
+    <h1>teste</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navBar',
+  components:{
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import '~@/scss/global.scss';
+  
+  .navbar{
+    @include themify(null, $themes) {
+      background-color: theme(primary);
+      
+    }
+  }
+</style>
